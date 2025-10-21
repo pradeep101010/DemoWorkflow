d8ba56bb21fb6260f29ac27a66bc0dd7b6fe2c14
pradeepsn10101 <pradeepsn10101@gmail.com>
2025-10-21 12:03:32 +0530

    demo

--- HttpExample/Index.js
+++ HttpExample/Index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send(`Hello, ${name}. This is your demo Node.js App running on Azure!`);
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
