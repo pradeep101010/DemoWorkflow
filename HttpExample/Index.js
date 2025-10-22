64bf5e040351c70408e39ad380c64abcfd65edbd
pradeepsn10101 <pradeepsn10101@gmail.com>
2025-10-22 11:44:40 +0530

    test

--- HttpExample/Index.js
+++ HttpExample/Index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send(`Hello, ${name}. This is your demo Node.js App running on Azure! And this is post CD modification `);
 });
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
